547e977e93e8a03b252753af8dc376e327b8cef9
Cristian Rodrigo Estrada Romero <ecristian963@gmail.com>
2022-04-19 17:02:19 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,348 @@
+<template>
+  <!-- Navigation -->
+  <div class="logo">
+    <i class="fa fa-book" aria-hidden="true"><span>Wiki UCC</span></i>
+  </div>
+  <a class="menu-toggle rounded" href="#">
+    <i class="fa fa-bars"></i>
+  </a>
+  <nav id="sidebar-wrapper">
+    <ul class="sidebar-nav">
+      <li class="sidebar-brand">
+        <a class="smooth-scroll" href="#Header"></a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#page-top">INICIO</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#About">CMMI</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Services">COBIT</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll"  href="#Moprosoft">MOPROSOFT</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Portfolio">ANEXOS</a>
+      </li>
+      <li class="sidebar-nav-item">
+        <a class="smooth-scroll" href="#Contact">BIBLIOGRAFIA</a>
+      </li>
+    </ul>
+  </nav>
+  <!-- Header Starts -->
+  <section id="Banner" class="content-section">
+    <div class="container content-wrap text-center">
+      <h1>¿Modelos de calidad de Software?</h1>
+      <h3>
+        <em>¡aqui encontraras un poco acerca de ello!</em>
+      </h3>
+      <a class="btn btn-primary btn-xl smooth-scroll" href="#About"
+        >Ingresa</a
+      >
+    </div>
+    <div class="overlay"></div>
+  </section>
+  <!-- Header Ends -->
+  <!-- CMMI  Starts -->
+  <section id="About" class="content-section text-center">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="block-heading">
+            <h2>CMMI (CAPABILITY MATURITY MODEL INTEGRATION)</h2>
+          </div>
+          <p class="lead">
+            Es una expansión del Modelo de Madurez (CMM). Consiste en una
+            herramienta de mejora de procesos que ayuda a las organizaciones a
+            optimizar la mejora de procesos, fomentando una cultura productiva y
+            eficiente que reduce los riesgos en el desarrollo de software,
+            productos y servicios
+          </p>
+          <br>
+          <p class="lead">
+            El modelo CMMI es muy importante ya que ayuda a no trabajar más de
+            lo necesario, sus técnicas ahorran tiempo y todo lo importante para
+            poder generar más rápidamente todas estas demandas. CMMI está
+            enfocado a la calidad en los procesos.
+          </p>
+          <br>
+          <h4>Estándar de Calidad</h4>
+          <br>
+          <p class="lead">
+            Con base a la medicion de la madurez de los proyectos, CMMI es
+            similar a la norma ISO 15504 SPICE.
+          </p>
+          <br>
+          <h4>
+            Empresas certificadas con CMMI nivel 5 en Colombia SoftManagement
+            (Bogotá – Colombia)
+          </h4>
+          <br>
+          <p class="lead">
+            En el nivel de madurez 5 (llamado también de optimización), nuestra
+            organización ha logrado todos los goalsof en el proceso y las zonas
+            asignadas a los niveles de madurez 2, 3, 4 y 5, y los objetivos
+            genéricos asignados a los niveles de madurez 2 y 3.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Cobit Us Starts -->
+  <section id="Services" class="content-section text-center">
+    <div class="container ">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="block-heading">
+            <h2>COBIT 5</h2>
+          </div>
+            <p class="lead">
+            Es una guía de mejores prácticas presentado como framework, dirigida al control y
+supervisión de tecnología de la información (TI) y es la última edición del framework
+mundialmente aceptado.
+COBIT es empleado en todo el mundo por quienes tienen como responsabilidad
+primaria los procesos de negocio y la tecnología, aquellos de quien depende la
+tecnología y la información confiable, y los que proveen calidad, confiabilidad y control
+de TI. COBIT 5 se basa en el gobierno de TI y el gobierno Corporativo.
+          </p>
+          <br>
+           <h4>COBIT una metodología orientada a procesos</h4>
+           <br>
+          <p class="lead">
+            COBIT define las actividades de TI en un modelo genérico de procesos organizado en
+cuatro dominios. Estos dominios son Planear y Organizar, Adquirir e Implementar,
+Entregar y Dar Soporte, Monitorear y Evaluar. Los dominios se equiparan a las áreas
+tradicionales de TI de planear, construir, ejecutar y monitorear.
+          </p>
+          <br>
+          <h4>Beneficios </h4>
+          <br>
+          <p class="lead">
+            <ul class="lead">Optimizar los servicios, el coste de las TI y la tecnologias.</ul>
+            <ul class="lead">Apoyar el cumplimiento de las leyes, reglamentos,acuerdos contractuales y las politicas</ul>
+            <ul class="lead">Gestion de nuevas tecnologias de información</ul>
+          </p>
+          <br>
+         
+        </div>
+      </div>
+<div class="container">
+        <br>
+        <br>
+      <div class="portfolio-wrapper clearfix">
+        <a
+          class="each-portfolio"
+          data-fancybox="gallery"
+          href="assets/images/COBIT5.png"
+        >
+          <div class="content hover-cont-wrap">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="assets/images/COBIT5.png" />
+            <div class="content-details fadeIn-bottom">
+              <h5 class="p-title">Title</h5>
+              <p class="p-desc">
+                Infografia COBIT5
+              </p>
+              <span class="zoom"><i class="fa fa-search-plus"></i></span>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+
+
+
+    </div>
+  </section>
+  <!--moprosoft section start-->
+<section id="Moprosoft" class="content-section text-center">
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="block-heading">
+            <h2>CMMI (CAPABILITY MATURITY MODEL INTEGRATION)</h2>
+          </div>
+          <p class="lead">
+            Es una expansión del Modelo de Madurez (CMM). Consiste en una
+            herramienta de mejora de procesos que ayuda a las organizaciones a
+            optimizar la mejora de procesos, fomentando una cultura productiva y
+            eficiente que reduce los riesgos en el desarrollo de software,
+            productos y servicios
+          </p>
+          <br>
+          <p class="lead">
+            El modelo CMMI es muy importante ya que ayuda a no trabajar más de
+            lo necesario, sus técnicas ahorran tiempo y todo lo importante para
+            poder generar más rápidamente todas estas demandas. CMMI está
+            enfocado a la calidad en los procesos.
+          </p>
+          <br>
+          <h4>Estándar de Calidad</h4>
+          <br>
+          <p class="lead">
+            Con base a la medicion de la madurez de los proyectos, CMMI es
+            similar a la norma ISO 15504 SPICE.
+          </p>
+          <br>
+          <h4>
+            Empresas certificadas con CMMI nivel 5 en Colombia SoftManagement
+            (Bogotá – Colombia)
+          </h4>
+          <br>
+          <p class="lead">
+            En el nivel de madurez 5 (llamado también de optimización), nuestra
+            organización ha logrado todos los goalsof en el proceso y las zonas
+            asignadas a los niveles de madurez 2, 3, 4 y 5, y los objetivos
+            genéricos asignados a los niveles de madurez 2 y 3.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--moprosoft section end-->
+  <section class="content-section text-center" id="Portfolio">
+    <div class="container">
+      <div class="block-heading">
+        <h2>MOPROSOST</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+      <div class="portfolio-wrapper clearfix">
+        <a
+          class="each-portfolio"
+          data-fancybox="gallery"
+          href="assets/images/p-two.jpeg"
+        >
+          <div class="content hover-cont-wrap">
+            <div class="content-overlay"></div>
+            <img class="content-image" src="assets/images/p-two.jpeg" />
+            <div class="content-details fadeIn-bottom">
+              <h5 class="p-title">Title</h5>
+              <p class="p-desc">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              </p>
+              <span class="zoom"><i class="fa fa-search-plus"></i></span>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+  <!-- TESTIMONIALS -->
+  
+  <section id="Contact" class="content-section">
+    <div class="container">
+      <div class="block-heading">
+        <h2>Contact Us</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </div>
+      <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+          <div class="contact-wrapper">
+            <div class="address-block border-bottom">
+              <h3 class="add-title">Headquaters</h3>
+              <div class="c-detail">
+                <span class="c-icon"
+                  ><i class="fa fa-map-marker" aria-hidden="true"></i></span
+                ><span class="c-info"> Street Addess</span>
+              </div>
+              <div class="c-detail">
+                <span class="c-icon"
+                  ><i class="fa fa-phone" aria-hidden="true"></i></span
+                ><span class="c-info">+0000000000</span>
+              </div>
+              <div class="c-detail">
+                <span class="c-icon"
+                  ><i class="fa fa-envelope" aria-hidden="true"></i></span
+                ><span class="c-info">email@yourdomain.com</span>
+              </div>
+            </div>
+            <div class="address-block">
+              <h3 class="add-title">Branch</h3>
+              <div class="c-detail">
+                <span class="c-icon"
+                  ><i class="fa fa-map-marker" aria-hidden="true"></i></span
+                ><span class="c-info"> 985698 Street</span>
+              </div>
+              <div class="c-detail">
+                <span class="c-icon"
+                  ><i class="fa fa-phone" aria-hidden="true"></i></span
+                ><span class="c-info">+0000000000</span>
+              </div>
+              <div class="c-detail">
+                <span class="c-icon"
+                  ><i class="fa fa-envelope" aria-hidden="true"></i></span
+                ><span class="c-info">email@yourdomain.com</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6">
+          <div class="form-wrap">
+            <form action="javascript:void(0)" method="post">
+              <div class="fname floating-label">
+                <input type="text" class="floating-input" name="full name" />
+                <label for="title">First Name</label>
+              </div>
+              <div class="fname floating-label">
+                <input type="text" class="floating-input" name="full name" />
+                <label for="title">Last Name</label>
+              </div>
+              <div class="email floating-label">
+                <input type="email" class="floating-input" name="email" />
+                <label for="email">Email</label>
+              </div>
+              <div class="contact floating-label">
+                <input type="tel" class="floating-input" name="contact" />
+                <label for="email">Mobile</label>
+              </div>
+              <div class="company floating-label">
+                <textarea
+                  type="text"
+                  class="floating-input"
+                  name="company"
+                ></textarea>
+                <label for="email">Message</label>
+              </div>
+              <div class="submit-btn">
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer class="footer text-center">
+    <div class="container">
+      <ul class="list-inline">
+        <li class="list-inline-item">
+          <a
+            class="social-link rounded-circle text-white mr-3"
+            href="javascript:void(0)"
+          >
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a
+            class="social-link rounded-circle text-white mr-3"
+            href="javascript:void(0)"
+          >
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a
+            class="social-link rounded-circle text-white"
+            href="javascript:void(0)"
+          >
+            <i class="fa fa-linkedin" aria-hidden="true"></i>
+          </a>
+        </li>
+      </ul>
+      <p class="text-muted small mb-0">Copyright © Your Website 2021</p>
+    </div>
+  </footer>
+</template>
